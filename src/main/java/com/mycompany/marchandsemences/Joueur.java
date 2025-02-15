1e35f2e7a8cbe76e9c02a3ea5497e4a6eb1667ed
nikolakisiov <nikolakisiov@localhost>
2025-02-14 19:21:07 -0500

    Added class attributes and initialization behavior for all classes.

---
+++ src/main/java/com/mycompany/marchandsemences/Joueur.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.marchandsemences;
+
+/**
+ *
+ * @author nikolakisiov
+ */
+public class Joueur {
+//    contient les infos du joueur
+
+    private double banque;
+
+    Joueur(double montant) {
+        banque = montant;
+    }
+
+}
